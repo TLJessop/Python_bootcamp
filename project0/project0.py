a14819e455fea6a947430b2b2d996ca856fa0ae5
Tljessop <tljessop@hotmail.com>
2021-09-29 15:43:05 -0700

    first basic python module

---
+++ project0/project0.py
@@ -0,0 +1,23 @@
+'''
+Project 0 Module
+'''
+
+class Project0:
+    '''
+    Documentation Class Project 0
+    '''
+
+    #method header
+    def run(self):
+        '''
+        What this method does
+        :return: none
+        '''
+        message = "hello world"
+        print(message)
+
+
+#Outside the class
+
+PROJECT_O_INSTANCE = Project0()
+PROJECT_O_INSTANCE.run()
